09dab68945c90b48687c29f8157131ba9f3b6482
LEV3457 <lukyanovivan83@gmail.com>
2025-04-04 21:45:11 +0300

    60

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import asyncio
 import os
 from loguru import logger
-from aiogram import Bot, Dispatcher, types, F
+from aiogram import Bot, Dispatcher, types, F, Router
 from aiogram.filters import Command
 from dotenv import find_dotenv, load_dotenv
 from aiogram.enums import ChatType
+
+
+router = Router()
 
 
 load_dotenv(find_dotenv())
@@ -13,10 +16,10 @@ TOKEN = os.getenv("TOKEN")
 
 async def main():
     logger.add("file.log",
-               format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
-               rotation="3 days",
-               backtrace=True,
-               diagnose=True)
+            format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
+            rotation="3 days",
+            backtrace=True,
+            diagnose=True)
 
     bot = Bot(TOKEN)
     logger.info("Бот создан")
@@ -35,11 +38,12 @@ async def main():
         await message.answer("держи подарочек")
         logger.info("Бот обьяснил, что делает")
 
-    @dp.message()
-    async def echo(message: types.Message):
-        await message.answer(message.text)  
-        logger.info(f"Бот вернул пользователю сообщение {message.text}")
+    # @dp.message()
+    # async def echo(message: types.Message):
+    #     await message.answer(message.text)
+    #     logger.info(f"Бот вернул пользователю сообщение {message.text}")
 
+    await dp.start_polling(bot)
 
 
     @router.message(
@@ -50,9 +54,34 @@ async def main():
         sender = message.from_user
         sender_username = f"@{sender.username}" if sender.username else sender.first_name
         await message.reply(f"{sender_username} нет конфет")
+
+
+    @router.message(
+        F.chat.type.in_([ChatType.GROUP, ChatType.SUPERGROUP]),
+        F.text.lower().contains("люблю")
+    )
+    async def interactive_love(message: types.Message):
+        await message.reply("А я вас нет")
+
+
+    @router.message(
+        F.chat.type.in_([ChatType.GROUP, ChatType.SUPERGROUP]),
+        F.text.lower().contains('дать')
+)
+    async def cry(message: types.Message):
+        sender = message.from_user
+        sender_username = f"@{sender.username}" if sender.username else sender.first_name
+        await message.reply(f"{sender_username} нет конфет")
     
     
-    await dp.start_polling(bot)
+
+    @router.message(
+        F.chat.type.in_([ChatType.GROUP, ChatType.SUPERGROUP]),
+        F.text.lower().contains("радоваться")
+    )
+    async def interactive_love(message: types.Message):
+        await message.reply(" ты радуешься")
+    
 
 
 if __name__ == '__main__':
